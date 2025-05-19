ad0b596f328c461e8834241547c43ae525714e85
mirJakub <slawomir.krzyzak@gmail.com>
2025-05-19 11:44:03 +0200

    removed css, added header bar buttons abd blocks

---
+++ src/app/components/SectionButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./SectionButton.module.css";
+
+interface SectionButtonProps {
+  /** The label to display inside the pill */
+  text: string;
+  /** Whether this button is “active”/selected */
+  selected: boolean;
+  /** Optional click handler */
+  onClick?: () => void;
+}
+
+export default function SectionButton({
+  text,
+  selected,
+  onClick,
+}: SectionButtonProps) {
+  return (
+    <button
+      type="button"
+      className={`${styles.button} ${selected ? styles.selected : ""}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
